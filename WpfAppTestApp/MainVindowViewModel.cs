842f4584846744aa6ce38fbd9f8c3e6035e5aace
itpark-team <46758840+itpark-team@users.noreply.github.com>
2023-06-17 12:59:14 +0300

    Добавьте файлы проекта.

---
+++ WpfAppTestApp/MainVindowViewModel.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfAppTestApp
+{
+    internal class MainVindowViewModel : INotifyPropertyChanged
+    {
+        private int a;
+        private int b;
+        private int result;
+
+        public int A
+        {
+            get { return a; }
+            set
+            {
+                a = value;
+                OnPropertyChanged("A");
+            }
+        }
+
+        public int B
+        {
+            get { return b; }
+            set
+            {
+                b = value;
+                OnPropertyChanged("B");
+            }
+        }
+
+        public int Result
+        {
+            get { return result; }
+            set
+            {
+                result = value;
+                OnPropertyChanged("Result");
+            }
+        }
+
+        private RelayCommand addCommand;
+        public RelayCommand AddCommand
+        {
+            get
+            {
+                return addCommand ??
+                  (addCommand = new RelayCommand(obj =>
+                  {
+                      Result = a + b;
+                  }));
+            }
+        }
+
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        public void OnPropertyChanged([CallerMemberName] string prop = "")
+        {
+            if (PropertyChanged != null)
+                PropertyChanged(this, new PropertyChangedEventArgs(prop));
+        }
+    }
+}
